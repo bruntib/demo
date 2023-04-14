01a052f857866efb5441eaf456fdddb2afed3da5
bruntib <bruntib@ik.elte.hu>
2023-04-15 00:02:42 +0200

    Replace the print operation to a function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-int main()
+void print_hello()
 {
   std::cout << "hello world" << std::endl;
+}
+
+int main()
+{
   for (int i = 1; i <= 20; ++i) 
   {
 	  std::cout << i << std::endl;
