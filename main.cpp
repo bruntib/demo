faa0f6a5bdbd410f9e550ab1a1c70b51e5e9eb2a
bruntib <bruntib@gmail.com>
2024-02-10 18:50:46 +0100

    Print the numbers.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+void printNumbers()
+{
+  for (int i = 0; i < 10; ++i)
+    std::cout << i << std::endl;
+}
+
 int main()
 {
   std::cout << "hello world" << std::endl;
+  printNumbers();
 }
